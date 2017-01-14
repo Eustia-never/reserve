8e91af68ccff275be7aaa19503f343b3aab26607
Eustia-never <greengreen213@gmail.com>
2017-01-14 09:48:03 +0900

    first-commit

---
+++ src/main/java/com/reso/ttp/checkreserv/MainApp.java
@@ -0,0 +1,34 @@
+package com.reso.ttp.checkreserv;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+
+    }
+
+    public void start(Stage stage) throws Exception {
+
+
+        String fxmlFile = "/fxml/main.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+
+        Scene scene = new Scene(rootNode, 400, 200);
+        scene.getStylesheets().add("/styles/styles.css");
+
+        stage.setTitle("イベント予約確認");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
